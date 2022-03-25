3ad8ca3a92ccfde8391adc0ba3dc3609f5f3d0f1
Thordis Kara Valsdottir <thor9706@stud.kea.dk>
2022-03-25 15:09:28 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+//WAITING FOR DOMCONTENTLOADED
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init(event) {
+  getData();
+}
+
+async function getData() {
+  let result = await fetch(
+    "http://thordiskara.com/recreate-bikeshop/wp-json/wp/v2/bike?_embed"
+  );
+  showBike(await result.json());
+}
+
+async function showBike(bikeArray) {
+  console.log(bikeArray);
+  const template = document.querySelector(".biketemplate").content;
+  const parentElement = document.querySelector("main");
+  bikeArray.forEach((bike) => {
+    const copy = template.cloneNode(true);
+    copy.querySelector("h4").textContent = bike.title.rendered;
+    copy.querySelector(".brand").textContent = bike.brand;
+    copy.querySelector(".priceinfo").textContent = bike.price;
+    copy.querySelector(".colorinfo").textContent = bike.colour;
+    copy.querySelector(".stockinfo").textContent = bike.stock;
+    copy.querySelector("img").src =
+      bike._embedded[
+        "wp:featuredmedia"
+      ][0].media_details.sizes.large.source_url;
+    parentElement.appendChild(copy);
+  });
+}
